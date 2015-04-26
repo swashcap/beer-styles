a13bd567a004d72e22c2727bc37e5f33ff26c9d1
Cory Reed <swashcap@gmail.com>
2015-04-26 14:49:11 -0700

    Add styles and scripts for 'Account' component.

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,32 @@
 /* jshint devel:true */
-console.log('\'Allo \'Allo!');
+(function (document, $) {
+  'use strict';
+
+  /**
+   * Account menu toggler.
+   */
+  var ACCOUNT_OPEN_CLASS = 'is-open';
+  var $accountTriggers = $('.account__trigger');
+
+  var closeAccountMenus = function () {
+    $accountTriggers.attr('aria-expanded', 'false');
+    $accountTriggers.parent().removeClass(ACCOUNT_OPEN_CLASS);
+  };
+  var toggleAccountMenu = function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    var $el = $(this);
+    var isActive = $el.attr('aria-expanded') === 'true';
+
+    if (! isActive) {
+      $el.parent().addClass(ACCOUNT_OPEN_CLASS);
+      $el.attr('aria-expanded', 'true');
+    } else {
+      closeAccountMenus();
+    }
+  };
+
+  $(document).on('click', closeAccountMenus);
+  $(document).on('click', '.account__trigger', toggleAccountMenu);
+})(document, jQuery);
